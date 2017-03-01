3153341152b160017bb1966a807786453e649ae8
偏右 <afc163@gmail.com>
2017-03-01 20:26:31 +0800

    Add antd Menu
    https://github.com/ant-design/ant-design/issues/5060

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,6 +9,7 @@ import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { DatePicker, Menu, Icon } from 'antd';
 
 import { makeSelectRepos, makeSelectLoading, makeSelectError } from 'containers/App/selectors';
 import H2 from 'components/H2';
@@ -22,7 +23,9 @@ import messages from './messages';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
 import { makeSelectUsername } from './selectors';
-import { DatePicker } from 'antd';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
@@ -77,6 +80,27 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
                   onChange={this.props.onChangeUsername}
                 />
                 <DatePicker />
+                <Menu mode="horizontal">
+                  <Menu.Item key="mail">
+                    <Icon type="mail" />Navigation One
+                  </Menu.Item>
+                  <Menu.Item key="app">
+                    <Icon type="appstore" />Navigation Two
+                  </Menu.Item>
+                  <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+                    <MenuItemGroup title="Item 1">
+                      <Menu.Item key="setting:1">Option 1</Menu.Item>
+                      <Menu.Item key="setting:2">Option 2</Menu.Item>
+                    </MenuItemGroup>
+                    <MenuItemGroup title="Item 2">
+                      <Menu.Item key="setting:3">Option 3</Menu.Item>
+                      <Menu.Item key="setting:4">Option 4</Menu.Item>
+                    </MenuItemGroup>
+                  </SubMenu>
+                  <Menu.Item key="alipay">
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+                  </Menu.Item>
+                </Menu>
               </label>
             </Form>
             <ReposList {...reposListProps} />
